1aef9493fffe0cf83aee25bdbbeb88b0c7bbade7
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-05-20 21:35:56 +0100

    Update loan_app.py

--- apps/loan_app.py
+++ apps/loan_app.py
@@ -1,1 +1,12 @@
+import streamlit as st
+import pandas as pd
+import base64
+import datetime
+import time
 
+
+def app():
+  # App Header
+st.markdown('''# **Loan Application Prediction**
+A Machine Learning Prediction Web App.
+''')
