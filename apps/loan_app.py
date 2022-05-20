7eb49369c6bd1d1defde2479dfd325348e48e09d
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-05-20 21:47:53 +0100

    Update loan_app.py

--- apps/loan_app.py
+++ apps/loan_app.py
@@ -10,3 +10,10 @@ def app():
   st.markdown('''# **Loan Application Prediction**
   A Machine Learning Prediction Web App.
   ''')
+  
+  # Side Note 1
+  expander_1 = st.sidebar.expander("PLEASE READ BEFORE YOU BEGIN")
+  expander_1.markdown("""<b>This App</b> has been designed out of passion to 
+  make <b>business intelligence</b> simple, easy, and efficient for business owners
+  with <b>no business analysis experience</b>. """, unsafe_allow_html=True)
+
