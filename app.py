fd1ae3d90ecf41aa3d0a8a437b5e5c55c33824ad
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-05-21 21:25:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, loan_app
+from apps import home, loan_app, loan_profile
 
 app = MultiApp()
 
 # add all applications
 app.add_app("Home Page", home.app)
 app.add_app("Loan App", loan_app.app)
+app.add_app("Customer Loan Profile", loan_profile.app)
 
 
 # The main app
