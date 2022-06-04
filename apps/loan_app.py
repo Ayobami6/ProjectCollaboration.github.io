a0fc85a6b03f147405fcb3203a0d8d7a01729cf5
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-06-04 22:22:11 +0100

    Update loan_app.py

--- apps/loan_app.py
+++ apps/loan_app.py
@@ -5,7 +5,6 @@ import pickle
 import datetime
 import base64
 import time
-from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
