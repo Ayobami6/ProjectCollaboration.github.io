52fa08e0390be5623deef8d916f5a7a69bc00bfe
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-06-19 20:26:31 +0100

    Update betaapp.py

--- apps/betaapp.py
+++ apps/betaapp.py
@@ -7,7 +7,7 @@ st.header("Loan Prediction App ")
 st.sidebar.markdown("💸 Beta Page")
 
 # Image For Page
-file_ = open("image.jpg", "rb")
+file_ = open("image.png", "rb")
 contents = file_.read()
 data_url = base64.b64encode(contents).decode("utf-8")
 file_.close()
